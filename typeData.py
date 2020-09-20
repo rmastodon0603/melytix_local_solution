3fd90c5df374b87eb252fbcfef17c22f07488330
Vladimir <rmastodon0603@gmail.com>
2020-09-20 12:55:48 +0300

    simple alert example

--- typeData.py
+++ typeData.py
@@ -6,7 +6,13 @@ ga_sessions = []
 with open(path, 'r') as f:
     data = json.loads(f.read())
     for element in range(len(data['ga_sessions'])):
-       	ga_sessions[element] = data['ga_sessions'][element]
+    	ga_sessions.append(data['ga_sessions'][element])
+    	#print(data['ga_sessions'][element])
+    	print("GA Session in day: " + str(data['ga_sessions'][element]))
 
-for i in ga_sessions:
-	print(i)
+if ga_sessions[0] >= ga_sessions[1]:
+	print("Hey, your traffic is good!")
+else:
+	print("Hey, your traffic is down!")
+
+
